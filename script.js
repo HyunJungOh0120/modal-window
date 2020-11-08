b9478068bf345e6d788a3eca71f7ca0eff4b0194
HyunJungOh0120 <72025960+HyunJungOh0120@users.noreply.github.com>
2020-11-09 00:10:31 +0800

    removed some html

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal'); // Array
-
+console.log(btnsOpenModal);
 ////////// EVENT HANDLER //////////////////////////////////
 const openModal = function (event) {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
+  console.log(modal);
 };
 const closeModal = function (event) {
   modal.classList.add('hidden');
@@ -19,14 +20,16 @@ const closeModal = function (event) {
 ///// Array ㅇㅣ기 때문에 .textContent 를 할 수가 없다. 그래서 for loop 사용해서 array 내 발류들을 꺼내주는 것! ///////////////////////////////////////////
 //  console.log(btnsOpenModal.textContent);    -> undefined
 
-for (let i = 0; i < btnsOpenModal.length; i++)
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(i);
   btnsOpenModal[i].addEventListener('click', openModal);
+}
 
+//////////////// Close Modal
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-/////////// Handling 'ESC' Keypress event //////////////////////////////////////////
-
+/////////// Handling 'ESC' Keypress event //
 document.addEventListener('keydown', function (event) {
   console.log(event.key);
 
