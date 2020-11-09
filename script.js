9db789c124f11e5cb6e7d4c2e4b08e46ebe6d133
HyunJungOh0120 <72025960+HyunJungOh0120@users.noreply.github.com>
2020-11-09 21:38:08 +0800

    each buttons triggers each modal(for...of Array Iterator)

--- script.js
+++ script.js
@@ -1,44 +1,59 @@
 'use strict';
 
 ////////// DOM Selector /////////////////////////////////////
-const btnsOpenModal = document.querySelectorAll('.show-modal'); // Array
+const btnsOpenModal = document.querySelectorAll('.show-modal'); //  위는  HTMLCollection Array
+// const btnarray = Array.from(btnsOpenModal);  -> not iteratable
 const btnOpenArray = Array.from(btnsOpenModal).entries();
-const modal = document.querySelector('.modal');
-const btnCloseModal = document.querySelector('.close-modal');
+const modals = document.querySelectorAll('.modal');
+const btnsCloseModal = document.querySelectorAll('.close-modal');
 const overlay = document.querySelector('.overlay');
 
-////////// EVENT HANDLER //////////////////////////////////
-const openModal = function (event) {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-  console.log(modal);
-};
-const closeModal = function (event) {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
+console.log(btnsOpenModal, btnOpenArray);
+console.log(modals);
 
-///// Array ㅇㅣ기 때문에 .textContent 를 할 수가 없다. 그래서 for loop 사용해서 array 내 발류들을 꺼내주는 것! ///////////////////////////////////////////
-//  console.log(btnsOpenModal.textContent);    -> undefined
-
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', openModal);
+// Then use 'for...of'-loop with the index of each item in 'btnOpenArray' for listening to a click event which toggles each modal to open and close
+for (let [index, button] of btnOpenArray) {
+  let buttonIndex = index;
+  function toggleModal() {
+    // Optionally toggle a class for CSS animations
+    modals[buttonIndex].classList.toggle('hidden');
+  }
+  button.addEventListener('click', toggleModal);
+  btnsCloseModal[buttonIndex].addEventListener('click', toggleModal);
 }
 
-btnsOpenModal.forEach(function (element) {
-  console.log(btnsOpenModal.element);
-  element.addEventListener('click', function () {});
-});
+// ////////// EVENT HANDLER //////////////////////////////////
+// const openModal = function (event) {
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+//   console.log(modal);
+// };
+// const closeModal = function (event) {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };
 
-//////////////// Close Modal
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+// ///// Array ㅇㅣ기 때문에 .textContent 를 할 수가 없다. 그래서 for loop 사용해서 array 내 발류들을 꺼내주는 것! ///////////////////////////////////////////
+// //  console.log(btnsOpenModal.textContent);    -> undefined
 
-/////////// Handling 'ESC' Keypress event //
-document.addEventListener('keydown', function (event) {
-  console.log(event.key);
+// for (let i = 0; i < btnsOpenModal.length; i++) {
+//   btnsOpenModal[i].addEventListener('click', openModal);
+// }
 
-  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
+// btnsOpenModal.forEach(function (element) {
+//   console.log(btnsOpenModal.element);
+//   element.addEventListener('click', function () {});
+// });
+
+// //////////////// Close Modal
+// btnCloseModal.addEventListener('click', closeModal);
+// overlay.addEventListener('click', closeModal);
+
+// /////////// Handling 'ESC' Keypress event //
+// document.addEventListener('keydown', function (event) {
+//   console.log(event.key);
+
+//   if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+//     closeModal();
+//   }
+// });
